aca857728d0d0d70980e4e576fb3a2940b45b193
Florian Deeken <florian@deeken.net>
2022-04-02 16:35:12 +0200

    added first asteroid, finished prototype ship

--- ship/ship.gd
+++ ship/ship.gd
@@ -1,5 +1,8 @@
 extends RigidBody
 
+var connected: bool = false
+var connected_asteroid: Node
+
 func _physics_process(delta):
 	var basis = global_transform.basis
 	if Input.is_action_pressed("ui_up"):
@@ -11,11 +14,32 @@ func _physics_process(delta):
 		apply_impulse(basis.xform($thruster_right1.translation), basis.xform($thruster_right1.thruster_vector))
 		apply_impulse(basis.xform($thruster_right2.translation), basis.xform($thruster_right2.thruster_vector))
 	if Input.is_action_pressed("ui_down"):
-		#apply_impulse($TF.position, Vector2(0, 10))
-		pass
+		apply_impulse(basis.xform($thruster_front.translation), basis.xform($thruster_front.thruster_vector))
 	if Input.is_action_pressed("ui_rotate_right"):
 		apply_impulse(basis.xform($thruster_right2.translation), basis.xform($thruster_right2.thruster_vector))
 		apply_impulse(basis.xform($thruster_left1.translation), basis.xform($thruster_left1.thruster_vector))
 	if Input.is_action_pressed("ui_rotate_left"):
 		apply_impulse(basis.xform($thruster_right1.translation), basis.xform($thruster_right1.thruster_vector))
 		apply_impulse(basis.xform($thruster_left2.translation), basis.xform($thruster_left2.thruster_vector))
+	if Input.is_action_just_pressed("ui_accept"):
+		if !connected and connected_asteroid != null:
+			$grabber/PinJoint.set_node_a(self.get_path())
+			$grabber2/PinJoint.set_node_a(self.get_path())
+			$grabber/PinJoint.set_node_b(connected_asteroid.get_path())
+			$grabber2/PinJoint.set_node_b(connected_asteroid.get_path())
+			connected = true;
+		elif connected:
+			$grabber/PinJoint.set_node_a(NodePath(""))
+			$grabber2/PinJoint.set_node_a(NodePath(""))
+			$grabber/PinJoint.set_node_b(NodePath(""))
+			$grabber2/PinJoint.set_node_b(NodePath(""))
+			connected = false;
+
+
+func _on_grabber_body_entered(body: Node):
+	print(body)
+	connected_asteroid = body
+
+func _on_grabber_body_exited(body: Node):
+	if (connected_asteroid == body):
+		connected_asteroid = null
