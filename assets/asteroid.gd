9493093cf6883d509773e5766706734647f7c29b
Florian Deeken <florian@deeken.net>
2022-04-03 21:49:53 +0200

    asteroid mass rework

--- assets/asteroid.gd
+++ assets/asteroid.gd
@@ -1,15 +1,16 @@
 class_name Asteroid
 extends RigidBody
 
-export(float) var min_size: float = 2
-export(float) var max_size: float = 3
+export(float) var min_size: float = 10
+export(float) var max_size: float = 30
 export(float) var max_rotate: float = 1
+export(float) var energy_mass_multiplyer: float = 1
 
 func _ready():
 	var sizef: float = rand_range(min_size, max_size)
-	var sizev: Vector3 = Vector3(sizef, sizef, sizef)
-	$Icosphere.scale = sizev
-	$CollisionShape.scale = sizev
+	$Icosphere.scale = sizef * Vector3(1,1,1)
+	$CollisionShape.scale = sizef * Vector3(1,1,1)
+	self.mass = self.mass * sizef
 	self.angular_velocity = Vector3(0, rand_range(0, max_rotate), 0)
 	
 func get_class(): return "Asteroid"
