2daa79444d2ec98aaaccedee11a1a272ff9e01d7
Florian Deeken <florian@deeken.net>
2022-04-03 18:22:42 +0200

    if the blackhole is to smale game ends

--- base/base.gd
+++ base/base.gd
@@ -6,11 +6,13 @@ export(float) var blackhole_max_radius: float = 25
 export(float) var blackhole_radius: float setget set_blackhol_size
 
 signal asteroid_entered
+signal destoryed
 
 
 func _ready():
 	self.blackhole_radius = blackhole_min_radius + (blackhole_max_radius-blackhole_min_radius) / 2
 	Global.set_backhole(self)
+	Global.connect("destroy_blackhole", self, "destory")
 	
 
 func set_blackhol_size(value: float):
@@ -29,3 +31,8 @@ func _on_blackhole_body_entered(body):
 	if body.get_class() != "Asteroid":
 		return
 	emit_signal("asteroid_entered", body)
+
+
+func destory():
+	print("kaboom !!!!!")
+	emit_signal("destoryed")
