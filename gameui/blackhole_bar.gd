f4f8670c9736b84dfb3373caeaf8c6e585b23a64
Florian Deeken <florian@deeken.net>
2022-04-05 00:21:32 +0200

    updated ui blackhole bar

--- gameui/blackhole_bar.gd
+++ gameui/blackhole_bar.gd
@@ -5,17 +5,15 @@ const width = 200
 export(float) var max_civ_energy: float = 1000
 export(float) var civ_energy: float = 500 setget set_civ_energy
 
-export(float) var max_blackhole_size: float = 28
-export(float) var blackhole_size: float = 19 setget set_blackhole_size
+export(float) var blackhole_size: float = 0.5 setget set_blackhole_size
 
 func _ready():
 	set_civ_energy(civ_energy)
 	set_blackhole_size(blackhole_size)
 	
 func _process(delta):
-	if (Global.blackhole_node != null):
-		self.blackhole_size = Global.blackhole_node.blackhole_radius
-		self.civ_energy = Global.civ_power
+	self.blackhole_size = 1 - Global.blackhole_factor
+	self.civ_energy = Global.civ_power
 
 
 func set_civ_energy(value: float):
@@ -25,5 +23,5 @@ func set_civ_energy(value: float):
 	
 func set_blackhole_size(value: float):
 	blackhole_size = value
-	var posx = width - (width / max_blackhole_size * blackhole_size)
+	var posx = width * blackhole_size
 	$bar.scale = Vector2(posx, 25)
