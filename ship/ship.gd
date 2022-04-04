b018d895578dd6e9ee76d03613f8cc7932c67f08
Florian Deeken <florian@deeken.net>
2022-04-04 16:37:25 +0200

    added thruster and blackhole sounds to game

--- ship/ship.gd
+++ ship/ship.gd
@@ -6,43 +6,43 @@ var connected_asteroid: Node
 func _physics_process(delta):
 	var basis = global_transform.basis
 	
-	$thruster_back/exhaust.visible = false
-	$thruster_front/exhaust.visible = false
-	$thruster_left1/exhaust.visible = false
-	$thruster_left2/exhaust.visible = false
-	$thruster_right1/exhaust.visible = false
-	$thruster_right2/exhaust.visible = false
-	$thruster_boost/exhaust.visible = false
+	$thruster_back.visible = false
+	$thruster_front.visible = false
+	$thruster_left1.visible = false
+	$thruster_left2.visible = false
+	$thruster_right1.visible = false
+	$thruster_right2.visible = false
+	$thruster_boost.visible = false
 	
 	if Input.is_action_pressed("ui_up"):
 		apply_impulse(basis.xform($thruster_back.translation), basis.xform($thruster_back.thruster_vector))
-		$thruster_back/exhaust.visible = true
+		$thruster_back.visible = true
 		if Input.is_action_pressed("ui_boost"):
 			apply_impulse(basis.xform($thruster_boost.translation), basis.xform($thruster_boost.thruster_vector))	
-			$thruster_boost/exhaust.visible = true
+			$thruster_boost.visible = true
 	if Input.is_action_pressed("ui_right"):
 		apply_impulse(basis.xform($thruster_right1.translation), basis.xform($thruster_right1.thruster_vector))
 		apply_impulse(basis.xform($thruster_right2.translation), basis.xform($thruster_right2.thruster_vector))
-		$thruster_right1/exhaust.visible = true
-		$thruster_right2/exhaust.visible = true
+		$thruster_right1.visible = true
+		$thruster_right2.visible = true
 	if Input.is_action_pressed("ui_left"):
 		apply_impulse(basis.xform($thruster_left1.translation), basis.xform($thruster_left1.thruster_vector))
 		apply_impulse(basis.xform($thruster_left2.translation), basis.xform($thruster_left2.thruster_vector))
-		$thruster_left1/exhaust.visible = true
-		$thruster_left2/exhaust.visible = true
+		$thruster_left1.visible = true
+		$thruster_left2.visible = true
 	if Input.is_action_pressed("ui_down"):
 		apply_impulse(basis.xform($thruster_front.translation), basis.xform($thruster_front.thruster_vector))
-		$thruster_front/exhaust.visible = true
+		$thruster_front.visible = true
 	if Input.is_action_pressed("ui_rotate_right"):
 		apply_impulse(basis.xform($thruster_right1.translation), basis.xform($thruster_right1.thruster_vector))
 		apply_impulse(basis.xform($thruster_left2.translation), basis.xform($thruster_left2.thruster_vector))
-		$thruster_right1/exhaust.visible = true
-		$thruster_left2/exhaust.visible = true
+		$thruster_right1.visible = true
+		$thruster_left2.visible = true
 	if Input.is_action_pressed("ui_rotate_left"):
 		apply_impulse(basis.xform($thruster_right2.translation), basis.xform($thruster_right2.thruster_vector))
 		apply_impulse(basis.xform($thruster_left1.translation), basis.xform($thruster_left1.thruster_vector))
-		$thruster_right2/exhaust.visible = true
-		$thruster_left1/exhaust.visible = true
+		$thruster_right2.visible = true
+		$thruster_left1.visible = true
 	if Input.is_action_just_pressed("ui_accept"):
 		if !connected and connected_asteroid != null:
 			$PinJoint.set_node_a(self.get_path())
@@ -58,6 +58,7 @@ func _physics_process(delta):
 			connected = false;
 
 
+
 func _on_grabber_body_entered(body: Node):
 	print(body)
 	connected_asteroid = body
@@ -65,3 +66,4 @@ func _on_grabber_body_entered(body: Node):
 func _on_grabber_body_exited(body: Node):
 	if (connected_asteroid == body):
 		connected_asteroid = null
+
--- ship/thruster.gd
+++ ship/thruster.gd
@@ -1,7 +1,21 @@
 extends Spatial
 
 export(Vector3) var thruster_vector
+export(NodePath) var audio setget set_audio
+var audio_node
+var old_vis: bool = false
 
 func _ready():
-	pass # Replace with function body.
+	if audio:
+		set_audio(audio)
+	
+func _process(delta):
+	if audio_node && self.visible != old_vis:
+		audio_node.playing = self.visible
+		old_vis = self.visible
+		
+func set_audio(value: NodePath):
+	audio = value
+	audio_node = get_node(audio)
+		
 
