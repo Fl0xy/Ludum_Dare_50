81ae994edfdf8b7f563506b84fe8e425f9517307
akronsus <no>
2022-04-05 02:21:47 +0200

    added mouse button support for titlescreen

--- TitleScreen.gd
+++ TitleScreen.gd
@@ -3,7 +3,7 @@ extends Spatial
 export var followup_scene : PackedScene = load("res://StartMenu.tscn")
 
 func _input(event):
-	if event is InputEventKey:
+	if event is InputEventKey or event is InputEventMouseButton:
 		var ins = followup_scene.instance()
 		get_tree().root.add_child(ins)
 		queue_free()
